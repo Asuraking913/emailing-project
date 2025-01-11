a2d43491d441ccf82c2b3b2b6023cef61c9cda7f
Israel Shedrack <israelshedrack913@gmail.com>
2025-01-11 21:16:28 +0100

    update: finished testing with html template

--- send_email/views.py
+++ send_email/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
+from email_example.settings import EMAIL_HOST_USER
 
 
 def send_email(sender, receiver):
@@ -15,13 +16,19 @@ def send_email(sender, receiver):
 # Create your views here.
 def index(request):
 
-    # send_email("emailtesting913@gmail.com", "israelshedrack913@gmail.com")
-    send_mail(
-        "Hello From Asura's Testing codes",
-        f"Hello there , you just received an automated email from israelshedrack913@gmail.com",
-        "emailtesting913@gmail.com",
-        ["emailtesting913@gmail.com"],
-        fail_silently=False,
-    )
+    if request.method == "POST":
+        sender_name = request.POST.get("name")
+        sender_email = request.POST.get("email")
+        sender_sms = request.POST.get("sms")
+
+        print(EMAIL_HOST_USER)
+
+        send_mail(
+            f"Test email from {EMAIL_HOST_USER}",
+            f"{sender_sms}",
+            f"{EMAIL_HOST_USER}",
+            [f"{sender_email}"],
+            fail_silently=False,
+        )
 
     return render(request, "index.html")
